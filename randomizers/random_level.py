6f4f1c9912bf0d75cf97a043a4ad9010db658747
scicluna <88948700+scicluna@users.noreply.github.com>
2024-03-05 22:59:26 -0500

    implemented random leveler

--- randomizers/random_level.py
+++ randomizers/random_level.py
@@ -1,2 +1,10 @@
+from random import randint
+
+
 def random_level(min: int, max: int) -> int:
-    ...
+    """Generates a random level between the min and max provided. If min is greater than max, the values are swapped."""
+
+    if min > max:
+        min, max = max, min
+
+    return randint(min, max)
